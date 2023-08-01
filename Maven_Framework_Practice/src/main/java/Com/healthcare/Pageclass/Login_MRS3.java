c4e7ee94e5516eb7e1af9011f437a613b491776c
admin <admin@DESKTOP-CQ3GOLO>
2023-08-01 22:32:17 +0530

    New change in login page

---
+++ Maven_Framework_Practice/src/main/java/Com/healthcare/Pageclass/Login_MRS3.java
@@ -0,0 +1,51 @@
+package Com.healthcare.Pageclass;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import Com.healthcare.Baseclass.BaseClass_OpenMRS;
+
+public class Login_MRS3 extends BaseClass_OpenMRS {
+
+	
+
+	public Login_MRS3() {
+
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(xpath = "//input[@id='username']")
+	public WebElement Username;
+
+	@FindBy(xpath = "//input[@placeholder=\"Enter your password\"]")
+	public WebElement Password;
+
+	@FindBy(xpath = "//li[@id='Inpatient Ward']")
+	public WebElement Ward_Tab;
+
+	@FindBy(xpath = "//input[@id='loginButton']")
+	public WebElement LoginButton;
+
+	public void Setusername(String U_Name) {
+
+		Username.sendKeys(U_Name);
+		//confirm password
+	}
+
+	public void SetPassword(String P_Pass) {
+
+		Password.sendKeys(P_Pass);
+	}
+
+	public void Wardclick() {
+
+		Ward_Tab.click();
+	}
+
+	public void Login_Click() {
+
+		LoginButton.click();
+	}
+
+}
